d06b1e38b711dd3cbbbd38a3ab8a60416279baa2
Johannes M. Schmitt <schmittjoh@gmail.com>
2012-05-06 17:47:11 -0500

    cleaned up twig.js extern

--- src/externs/twig.js
+++ src/externs/twig.js
@@ -18,6 +18,13 @@ twig.Environment = function() {};
 twig.Environment.prototype.render = function(arg1, opt_arg2) {};
 
 /**
+ * @param {Function} templateCtor
+ * @param {string} macroName
+ * @param {...*} var_args
+ */
+twig.Environment.prototype.macro = function(templateCtor, macroName, var_args) { };
+
+/**
  * @param {Function} arg1
  * @return {twig.Template}
  */
@@ -28,23 +35,3 @@ twig.Environment.prototype.createTemplate = function(arg1) {};
  * @param {twig.Environment} arg1
  */
 twig.Template = function(arg1) {};
-
-
-var profile = {};
-
-/** 
- * @constructor
- * @param {twig.Environment} arg1
- * @extends {twig.Template}
- */
-profile.macros = function(arg1) {};
-
-/**
- * @return {string}
- */
-profile.macros.prototype.getnoTargetTooltip = function() {};
-
-/**
- * @return {string}
- */
-profile.macros.prototype.getnoFbSettings = function() {};
